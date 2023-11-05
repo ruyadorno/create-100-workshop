2e415ad759956534b9bfd441b74bd58083a09f69
Ruy Adorno <ruyadorno@google.com>
2023-11-05 22:45:56 +0000

    Add snapshot-based test on step1

--- template/step-1/test/index.js
+++ template/step-1/test/index.js
@@ -2,11 +2,7 @@ import t from 'tap'
 import { main } from '../src/index.js'
 
 const result = await main({ npm: '>=10.2' })
-t.match(
+t.matchSnapshot(
   result,
-  [
-    { version: 'v21.1.0' },
-    { version: 'v21.0.0' },
-  ],
-  'should have expected version'
+  'should have expected versions'
 )
